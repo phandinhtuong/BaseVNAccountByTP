e2db39bbf00a8e2d84402b666cddd003e566cad3
phandinhtuong <phandinhtuong25@gmail.com>
2025-07-22 14:58:05 +0700

    add logs: logout

--- public/Logout.php
+++ public/Logout.php
@@ -2,11 +2,23 @@
 
 session_start();
 
+error_log("----------------------------------------------------------------");
+error_log("begin logout");
+
+
+error_log("SERVER VARIABLES:\n" . print_r($_SERVER, true));
+error_log("SESSION VARIABLES:\n" . print_r($_SESSION, true));
+error_log("POST VARIABLES:\n" . print_r($_POST, true));
+
+
 // Unset all session variables
 $_SESSION = array();
 
 // Destroy the session
 session_destroy();
+
+error_log("end logout");
+error_log("----------------------------------------------------------------");
 
 // Redirect to login page
 //header("Location: login.php?error=logout");
